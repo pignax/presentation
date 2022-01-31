84252deb584fd3d48133160f7b834f0a68df816d
Giovanni Pignataro <giovannipignataro@MacBook-Pro-di-Giovanni.local>
2022-01-31 23:42:26 +0100

    start and welcome page

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import '../../style/index.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-logo">GP</h1>
+        <Link className="App-link" to="/presentation">PRESENTAZIONE</Link>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Presentation/Presentation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../style/index.css';
+
+function Presentation() {
+  return (
+    <div className="App">
+      Ciaoo
+    </div>
+  );
+}
+
+export default Presentation;
